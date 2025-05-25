3e3214136400853e2cba3c0d6e5224b4b0137744
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-05-25 19:32:08 -0300

    incluido o método CBV no código

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path
-from . import views
+from .views import (
+    IndexView, ContatoView, ProdutoDetailView, DetalheProdutoView,
+    RegistrarUsuarioView, ListarPedidosView, ListarItensPedidoView,
+    ListarAvaliacoesView, LogarUsuarioView, DeslogarUsuarioView,
+    adicionar_ao_carrinho, ver_carrinho, remover_do_carrinho
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Página inicial
-    path('home/', views.index, name='home'),  # Adiciona o nome 'home' para a mesma view
-    path('contato', views.contato, name='contato'),
-    path('produto/<int:pk>', views.produto, name='produto'),
-    path('produto/detalhe/<int:pk>/', views.detalhe_produto, name='detalhe_produto'),
-    path('registro/', views.registrar_usuario, name='registro'),
-    path('login/', views.logar_usuario, name='login'),
-    path('logout/', views.deslogar_usuario, name='logout'),
-    path('avaliacoes/', views.listar_avaliacoes, name='listar_avaliacoes'),
-    path('adicionar_ao_carrinho/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
-    path('carrinho/', views.ver_carrinho, name='ver_carrinho'),
-    path('remover_do_carrinho/<int:produto_id>/', views.remover_do_carrinho, name='remover_do_carrinho'),
+    path('', IndexView.as_view(), name='index'),
+    path('home/', IndexView.as_view(), name='home'),
+    path('contato', ContatoView.as_view(), name='contato'),
+    path('produto/<int:pk>', ProdutoDetailView.as_view(), name='produto'),
+    path('produto/detalhe/<int:pk>/', DetalheProdutoView.as_view(), name='detalhe_produto'),
+    path('registro/', RegistrarUsuarioView.as_view(), name='registro'),
+    path('pedidos/', ListarPedidosView.as_view(), name='listar_pedidos'),
+    path('itens_pedido/', ListarItensPedidoView.as_view(), name='listar_itens_pedido'),
+    path('avaliacoes/', ListarAvaliacoesView.as_view(), name='listar_avaliacoes'),
+    path('login/', LogarUsuarioView.as_view(), name='login'),
+    path('logout/', DeslogarUsuarioView.as_view(), name='logout'),
+    path('adicionar_ao_carrinho/<int:produto_id>/', adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
+    path('carrinho/', ver_carrinho, name='ver_carrinho'),
+    path('remover_do_carrinho/<int:produto_id>/', remover_do_carrinho, name='remover_do_carrinho'),
 ]
 
--- core/views.py
+++ core/views.py
@@ -2,89 +2,79 @@ from django.shortcuts import render, redirect, get_object_or_404 # type: ignore
 
 from core.models import Produto
 from .forms import RegistroForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.db import models
 from core.models import Pedido, ItemPedido, Avaliacao
+from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
 
+class IndexView(ListView):
+    model = Produto
+    template_name = 'core/index.html'
+    context_object_name = 'produtos'
 
-def index(request):
-    produtos = Produto.objects.all()
-    context = {
-        'curso': "Web progamming with Django Framework",
-        'other': "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of Web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.",
-        'other2': "Projeto de site de vendas com Django em andamento, por favor, aguarde...",
-        'produtos' : produtos
-    }
-    return render(request, 'core/index.html' , context)
 
-def contato(request):
-    return render(request, 'core/contato.html')
+class ContatoView(TemplateView):
+    template_name = 'core/contato.html'
 
-def produto(request, pk):
-    try:
-        prod = Produto.objects.get(id=pk)
-        context = {
-            'produto': prod
-        }
-        return render(request, 'core/produto.html', context)
-    except Produto.DoesNotExist:
-        # Renderiza uma página fictícia para produtos inexistentes
-        return render(request, 'core/produto_nao_encontrado.html')
+class ProdutoDetailView(DetailView):
+    model = Produto
+    template_name = 'core/produto.html'
+    context_object_name = 'produto'
 
-def registrar_usuario(request):
-    if request.method == 'POST':
-        form = RegistroForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, f'Conta criada com sucesso para {username}!')
-            return redirect('index')
-    else:
-        form = RegistroForm()
-    return render(request, 'core/registrar_usuario.html', {'form': form})
+class DetalheProdutoView(DetailView):
+    model = Produto
+    template_name = 'core/detalhe_produto.html'
+    context_object_name = 'produto'
 
-def logar_usuario(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('index')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'core/login.html', {'form': form})
+class RegistrarUsuarioView(FormView):
+    template_name = 'core/registro.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
 
-def deslogar_usuario(request):
-    logout(request)
-    return redirect('index')
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-def listar_pedidos(request):
-    # Consulta otimizada com select_related para buscar os clientes relacionados
-    pedidos = Pedido.objects.com_cliente()
-    return render(request, 'core/listar_pedidos.html', {'pedidos': pedidos})
+class ListarPedidosView(ListView):
+    model = Pedido
+    template_name = 'core/listar_pedidos.html'
+    context_object_name = 'pedidos'
 
-def listar_itens_pedido(request):
-    # Consulta otimizada com select_related para buscar pedidos e produtos relacionados
-    itens = ItemPedido.objects.com_relacionados()
-    return render(request, 'core/listar_itens_pedido.html', {'itens': itens})
+    def get_queryset(self):
+        return Pedido.objects.com_cliente()
 
-def listar_avaliacoes(request):
-    query = request.GET.get('q')  # Obtém o termo de pesquisa do formulário
-    if query:
-        # Filtra as avaliações pelo nome do cliente ou do produto
-        avaliacoes = Avaliacao.objects.com_relacionados().filter(
-            models.Q(cliente__nome__icontains=query) | models.Q(produto__nome__icontains=query)
-        )
-    else:
-        # Retorna todas as avaliações se não houver pesquisa
-        avaliacoes = Avaliacao.objects.com_relacionados()
-    return render(request, 'core/listar_avaliacoes.html', {'avaliacoes': avaliacoes})
+class ListarItensPedidoView(ListView):
+    model = ItemPedido
+    template_name = 'core/listar_itens_pedido.html'
+    context_object_name = 'itens'
+
+    def get_queryset(self):
+        return ItemPedido.objects.com_relacionados()
+
+class ListarAvaliacoesView(ListView):
+    model = Avaliacao
+    template_name = 'core/listar_avaliacoes.html'
+    context_object_name = 'avaliacoes'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Avaliacao.objects.com_relacionados().filter(
+                models.Q(cliente__nome__icontains=query) | models.Q(produto__nome__icontains=query)
+            )
+        return Avaliacao.objects.com_relacionados()
+
+class LogarUsuarioView(LoginView):
+    template_name = 'core/login.html'
+
+class DeslogarUsuarioView(LogoutView):
+    next_page = 'index'
 
 
 def adicionar_ao_carrinho(request, produto_id):
@@ -148,7 +138,3 @@ def remover_do_carrinho(request, produto_id):
         request.session.modified = True # Sinaliza modificação
         messages.info(request, 'Item removido do carrinho.')
     return redirect('ver_carrinho') # Redireciona para a página do carrinho
-
-def detalhe_produto(request, pk):
-    produto = get_object_or_404(Produto, pk=pk)
-    return render(request, 'core/detalhe_produto.html', {'produto': produto})
